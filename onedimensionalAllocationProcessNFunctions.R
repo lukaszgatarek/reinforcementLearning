3dfd3bf9a7cfb9d9c543e67fb41143b7afddd958
Lukasz Gatarek <gatarek@tlen.pl>
2018-07-30 00:43:46 +0200

    some commenting
    .

--- onedimensionalAllocationProcessNFunctions.R
+++ onedimensionalAllocationProcessNFunctions.R
@@ -10,7 +10,7 @@ b <- matrix(N, 1)
 # consider a case with two function
 a[1] <- 1
 a[2] <- 2.5
-a[3] <- 3.3
+a[3] <- 3.8
 
 b[1] <- -1
 b[2] <- -4
@@ -28,13 +28,14 @@ R <- length(X)
 f <- matrix(NA, R, N)
 xnMaximizingFunction <- matrix(NA, R, N)
 
-# at the beginning we assume everything goes to the 1st function
+# at the beginning we assume that the full value of each x goes to the 1st function
 xnMaximizingFunction[,1] <- X
-
+# we evaluate the first function for each x
 f[,1] <- g(X, a[1], b[1])
 
+# we are iterating over the number of function type, so components in the sum
 for ( k in 2:N){
-  
+  # ... and the number of values to evaluate the function in the domain
   for (l in 1:R){
     
     print(paste('function evaluated for for x = ', toString(X[l])) )
@@ -43,10 +44,12 @@ for ( k in 2:N){
     
     auxiliaryRecord <- matrix(NA, l , 1)
     
+    # ... and for each value x[l] we check all the possible combination of g(d*delta, k) + f(x-d*delta, k-1), 
+    # ...where k-1 corresponds to the value of function achieved over the types 1 to k-1
     for (d in 0 : ( l - 1 ) ){
       
       print(paste('we check x_n = ', toString(k * Delta) ) )
-      # which(X == x - k * Delta) denotes the index giving the proper value of function f associated with x - k * Delta
+      # which(X == x - d * Delta) denotes the index giving the proper value of function f associated with x - d * Delta
       if ( (x - d * Delta) %in% X){
         auxiliaryRecord[d + 1, 1] <- g(d * Delta, a[k], b[k]) + f[which(X == x - d * Delta ), k-1]
       }
